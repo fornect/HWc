32976f46a463cf2d15e4f5dc1f86f68547538165
Глеб Мельник <melnikgleb113@gmail.com>
2025-10-14 10:58:11 +0300

    add Home Work 3

--- src/HW3/zero.c
+++ src/HW3/zero.c
@@ -8,15 +8,12 @@ int main()
     printf("введите количество элементов массива:");
     scanf("%d", &n);
     int a[n];
-    for (i = 0; i<n; i++)
-    {
+    for (i = 0; i < n; i++){
         printf("a[%d]=", i);
         scanf("%d", &a[i]);
     }
-    for (i = 0; i<n; i++)
-    {
-        if (a[i] == 0)
-        {
+    for (i = 0; i < n; i++){
+        if (a[i] == 0){
             k++;
         }
     }
