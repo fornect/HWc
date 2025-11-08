0ca762e0459fb2e9a69f3b66a91191d3416f8438
Глеб Мельник <melnikgleb113@gmail.com>
2025-11-08 18:11:26 +0300

    Add CMake for balansPlus

---
+++ src/HW5/build/CMakeFiles/balansPlus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/balansPlus.dir/link.d"
+  "CMakeFiles/balansPlus.dir/balansPlus.c.o"
+  "CMakeFiles/balansPlus.dir/balansPlus.c.o.d"
+  "balansPlus"
+  "balansPlus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/balansPlus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
